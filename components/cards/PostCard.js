410ceca0e29b30da68cb364094d617fee077ef97
B33blebroxx <b33blebroxx@gmail.com>
2023-11-25 21:18:03 -0600

    Created PostCard component and added files

---
+++ components/cards/PostCard.js
@@ -0,0 +1,39 @@
+import { Button, Card, ListGroup } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { useAuth } from '../../utils/context/authContext';
+import { deletePost, getAllPosts } from '../../api/postData';
+
+export default function PostCard({ profileObj, postObj }) {
+  const deletePostPrompt = () => {
+    if (window.confirm('Are you sure you want to delete this post?')) {
+      deletePost(postObj.firebaseKey).then(getAllPosts);
+    }
+  };
+  const { user } = useAuth();
+  const isCurrentUserProfile = user.uid === profileObj.uid;
+
+  return (
+    <div id="post-card-container">
+      <Card id="post-card">
+        <Card.Header id="post-header">Title: {postObj.title}</Card.Header>
+        <Card.Body>
+          <Card.Img id="post-img">{postObj.postImg}</Card.Img>
+          <ListGroup>
+            {isCurrentUserProfile && (<><Button variant="danger" onClick={deletePostPrompt}>Delete Post</Button><a href={`/post/update/${postObj.firebaseKey}`}><Button variant="info"> Edit Profile </Button></a></>)}
+          </ListGroup>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+PostCard.propTypes = {
+  profileObj: PropTypes.shape({
+    uid: PropTypes.string,
+  }).isRequired,
+  postObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    postImg: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+};
